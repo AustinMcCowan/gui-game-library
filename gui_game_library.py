a6138b78a7a58b3754ad043d2a44a9fa87d50854
AustinMcCowan <amccowan@stcharlessd.org>
2020-02-10 12:12:06 -0600

    new file:   gui_game_library.py

---
+++ gui_game_library.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# Austin McCowan
+# 2/10/2020
+
+'''gui program for/based on game_library'''
+
+import pickle
+import data_reboot as dr
+
+class Library(object):
+    def __init__(self):
+        
+        # Try and open the pickle file and grab contents
+        try: 
+            datafile = open("game_lib.pickle", "rb")
+            self.games = pickle.load(datafile)
+            
+        # If unable to grab content, run the function inside of data_reboot.py. Then try loading content again.    
+        except:            
+            dr.fix_it()
+            print("Data failed to load; Grabbing original content")
+            datafile = open("game_lib.pickle", "rb")
+            self.games = pickle.load(datafile)
+        
+        # Close datafile    
+        datafile.close()
