2fa2996288205b4516de12a4b99090e2ec758044
AustinMcCowan <amccowan@stcharlessd.org>
2020-02-10 13:37:34 -0600

    Added buttons

--- gui_game_library.py
+++ gui_game_library.py
@@ -38,6 +38,20 @@ class MainMenu(tk.Frame):
         self.lbl_title = tk.Label(text= "Game Library", font = TITLE_FONT)
         self.lbl_title.grid(row = 0, column = 0, sticky = "news")
         
+        self.btn_add = tk.Button(text = "Add", font = BUTTON_FONT)
+        self.btn_add.grid(row = 1, column = 0)
+        
+        self.btn_edit = tk.Button(text = "Edit", font = BUTTON_FONT)
+        self.btn_edit.grid(row = 2, column = 0)
+        
+        self.btn_search = tk.Button(text = "Search", font = BUTTON_FONT)
+        self.btn_search.grid(row = 3, column = 0)
+        
+        self.btn_remove = tk.Button(text = "Remove", font = BUTTON_FONT)
+        self.btn_remove.grid(row = 4, column = 0)
+        
+        self.btn_save = tk.Button(text = "Save", font = BUTTON_FONT)
+        self.btn_save.grid(row = 5, column = 0)        
         
 # Functions/global functions
 
