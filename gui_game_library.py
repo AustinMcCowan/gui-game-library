2aacba15fa59dfc55d68d9886dcc6879954339af
AustinMcCowan <amccowan@stcharlessd.org>
2020-02-12 12:02:18 -0600

    Modified search menu

--- gui_game_library.py
+++ gui_game_library.py
@@ -47,62 +47,66 @@ class MainMenu(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
         self.lbl_title = tk.Label(self, text= "Game Library", font = TITLE_FONT)
-        self.lbl_title.grid(row = 0, column = 0, sticky = "news")
+        self.lbl_title.grid(row = 0, column = 0, columnspan=3, sticky = "news")
         
         self.btn_add = tk.Button(self, text = "Add", font = BUTTON_FONT)
-        self.btn_add.grid(row = 1, column = 0)
+        self.btn_add.grid(row = 1, column = 1)
         
         self.btn_edit = tk.Button(self, text = "Edit", font = BUTTON_FONT)
-        self.btn_edit.grid(row = 2, column = 0)
+        self.btn_edit.grid(row = 2, column = 1)
         
         self.btn_search = tk.Button(self, text = "Search", font = BUTTON_FONT)
-        self.btn_search.grid(row = 3, column = 0)
+        self.btn_search.grid(row = 3, column = 1)
         
         self.btn_remove = tk.Button(self, text = "Remove", font = BUTTON_FONT)
-        self.btn_remove.grid(row = 4, column = 0)
+        self.btn_remove.grid(row = 4, column = 1)
         
         self.btn_save = tk.Button(self, text = "Save", font = BUTTON_FONT)
-        self.btn_save.grid(row = 5, column = 0)        
+        self.btn_save.grid(row = 5, column = 1)        
         
 class SearchFilters(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, master=parent)
         self.lbl_print_filers = tk.Label(self, text="Print Filters")
-        self.lbl_print_filers.grid(row=0, column=0, columnspan=6)
+        self.lbl_print_filers.grid(row=0, column=0, columnspan=3)
         
-        self.chk_genre = tk.Checkbutton(self)
+        self.chk_genre = tk.Checkbutton(self, text="Genre")
         self.chk_genre.grid(row=1, column=0)
             
-        self.chk_title = tk.Checkbutton(self)
+        self.chk_title = tk.Checkbutton(self, text="Title")
         self.chk_title.grid(row=2, column=0)
         
-        self.chk_developer = tk.Checkbutton(self)
+        self.chk_developer = tk.Checkbutton(self, text="Developer")
         self.chk_developer.grid(row=3, column=0)
         
-        self.chk_publisher = tk.Checkbutton(self)
+        self.chk_publisher = tk.Checkbutton(self, text="Publisher")
         self.chk_publisher.grid(row=4, column=0) 
         
-        self.chk_system = tk.Checkbutton(self)
-        self.chk_system.grid(row=1, column=2) 
+        self.chk_system = tk.Checkbutton(self, text="System")
+        self.chk_system.grid(row=1, column=1) 
         
-        self.chk_release = tk.Checkbutton(self)
-        self.chk_release.grid(row=2, column=2) 
+        self.chk_release = tk.Checkbutton(self, text= "Release Date")
+        self.chk_release.grid(row=2, column=1) 
         
-        self.chk_rating = tk.Checkbutton(self)
-        self.chk_rating.grid(row=3, column=2) 
+        self.chk_rating = tk.Checkbutton(self, text="Rating")
+        self.chk_rating.grid(row=3, column=1) 
         
-        self.chk_mode = tk.Checkbutton(self)
-        self.chk_mode.grid(row=4, column=2) 
+        self.chk_mode = tk.Checkbutton(self, text="Game Mode")
+        self.chk_mode.grid(row=4, column=1) 
         
-        self.chk_price = tk.Checkbutton(self)
-        self.chk_price.grid(row=1, column=4) 
+        self.chk_price = tk.Checkbutton(self, text="Price")
+        self.chk_price.grid(row=1, column=2) 
         
-        self.chk_beat = tk.Checkbutton(self)
-        self.chk_beat.grid(row=2, column=4) 
+        self.chk_beat = tk.Checkbutton(self, text="Beat it?")
+        self.chk_beat.grid(row=2, column=2) 
         
-        self.chk_purchase = tk.Checkbutton(self)
-        self.chk_purchase.grid(row=3, column=4)     
+        self.chk_purchase = tk.Checkbutton(self, text="Purchase Date")
+        self.chk_purchase.grid(row=3, column=2)     
         
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+               
 class Search(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
@@ -164,7 +168,20 @@ class Remove(tk.Frame):
         self.btn_cancel.grid(row=2, column=0)
         
         self.btn_remove = tk.Button(self, text="Remove", font=BUTTON_FONT)
-        self.btn_remove.grid(row=2, column=1)        
+        self.btn_remove.grid(row=2, column=1)
+
+class Save_Message(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.lbl_saved = tk.Label(self, text="File saved", font=TITLE_FONT)
+        self.btn_okay = tk.Button(self, text="OK", font=BUTTON_FONT)
+    
+class Editor(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        
+        
+        
 # Functions/global functions
 
 
@@ -183,5 +200,9 @@ if __name__ == "__main__":
     edit_menu = Edit()
     edit_menu.grid(row=0, column=0, sticky='news')
     
+    remove_menu = Remove()
+    remove_menu.grid(row=0, column=0, sticky='news')
+    
+    root.grid_columnconfigure(0, weight=1)
     search_menu.tkraise()
     root.mainloop()
